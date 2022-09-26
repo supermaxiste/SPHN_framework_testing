c87c350dcca6f74581bff1376e79ec3ca83f6a44
Stefan Milosavljevic <supermegaiperste@hotmail.com>
2022-09-26 19:11:20 +0200

    Add script to download synthea and run simulation for 100 patients

---
+++ 01_simulation/patient_simulation.sh
@@ -0,0 +1,3 @@
+wget https://github.com/synthetichealth/synthea/releases/download/master-branch-latest/synthea-with-dependencies.jar .
+
+java -jar synthea-with-dependencies.jar -s 1663926070850 -r 20220923 -cs 1663926070850 -p 100 -a 20-90
